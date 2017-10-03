85fdc60483ff0f93017bec872ad1653a28717525
belyaev-ay <fxdev-belyaev-ay@fxclub.org>
2017-10-03 10:04:57 +0700

    delete old forms

--- public/js/controllers/connectionDialog.js
+++
@@ -1,8 +0,0 @@
-function connectionDialogController(connection, $scope, $uibModalInstance) {
-    $scope.connection = {
-        url: "ws://f2g.avvero.pw:61614/stomp"
-    }
-    $scope.ok = function () {
-        $uibModalInstance.close($scope.connection);
-    }
-}
