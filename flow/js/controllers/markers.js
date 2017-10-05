519a6746d0166cbd7abeac3d532ef542db7c4196
belyaev-ay <fxdev-belyaev-ay@fxclub.org>
2017-10-06 00:03:06 +0700

    connection history

--- flow/js/controllers/markers.js
+++ flow/js/controllers/markers.js
@@ -1,4 +1,5 @@
-function markersController($scope, page, config, $stateParams, $stompclient, Notification, $window) {
+function markersController($scope, page, config, $stateParams, $stompclient, Notification, $window, 
+    localStorageService) {
     page.setTitle('WMQ client')
 
     $scope.config = config
@@ -8,6 +9,7 @@ function markersController($scope, page, config, $stateParams, $stompclient, Not
         $stompclient.connected = true
         $stompclient.endpoint = connection.url
         $scope.connection = connection
+        $scope.saveConnectionsHistory(connection.url)
     }
     $scope.errorCallback = function(error){
         $stompclient.connected = false
@@ -23,7 +25,39 @@ function markersController($scope, page, config, $stateParams, $stompclient, Not
         $stompclient.connect(url, function(){$scope.callback({url: url})}, $scope.errorCallback)
     }
     $scope.subscribe = function (endpoint) {
+        $scope.saveEndpointsHistory(endpoint)
         $window.location.href = "#follow/" + endpoint
+    }
+    $scope.contains = function (list, entry) {
+        for (var i = 0; i < list.length; i++) {
+            if (entry == list[i]) {
+                return true
+            }        
+        }
+        return false
+    }
+    $scope.saveConnectionsHistory = function(newEntry) {
+        if (!$scope.contains($scope.connectionsHistory, newEntry)) {
+            $scope.connectionsHistory.unshift(newEntry)    
+        }
+        localStorageService.set("connectionsHistory", $scope.connectionsHistory)
+    }
+    $scope.connectionsHistory = localStorageService.get("connectionsHistory")
+    if (!$scope.connectionsHistory) {
+        $scope.connectionsHistory = []
+        // for (var i = 0; i < $scope.config.connections.length; i++) {
+        //     $scope.saveConnectionsHistory($scope.config.connections[i].url)
+        // }
+    }
+    $scope.saveEndpointsHistory = function(newEntry) {
+        if (!$scope.contains($scope.endpointsHistory, newEntry)){
+            $scope.endpointsHistory.unshift(newEntry)    
+        }
+        localStorageService.set("endpointsHistory", $scope.endpointsHistory)
+    }
+    $scope.endpointsHistory = localStorageService.get("endpointsHistory")
+    if (!$scope.endpointsHistory) {
+        $scope.endpointsHistory = []
     }
 }
 
